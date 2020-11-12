119d0a31d1463f9dee522a8207f118cbc1f738ca
raviteja.kvns <raviteja.kvns@gmail.com>
2020-11-12 09:58:34 +0000

    Correcting the paths

--- tools/data_utils/ImageSetGenerators/kitti-odometry/generate_for_selected_seq.py
+++ tools/data_utils/ImageSetGenerators/kitti-odometry/generate_for_selected_seq.py
@@ -10,34 +10,40 @@ def get_file_name(file):
 # Might need to maintain different file names eg. train_0,4.txt
 # Generating train.txt file with the given sequences
 
+base_path = "../../../..//data/kitti-odometry/"
+
 # removing existing file
-os.remove('../ImageSets/train.txt')
+train_file_path = base_path + 'ImageSets/train.txt'
+if os.path.exists(train_file_path):
+    os.remove(train_file_path)
 
 for seq in selected_training_seq:
 
-    seq_path = "../training/velodyne/%s/"%(seq)
+    seq_path = base_path + "training/velodyne/%s/"%(seq)
     files_list = list(map(get_file_name, os.listdir(seq_path)))
 
     np_arr = np.empty((len(files_list), 2), dtype="object")
     np_arr[:, 0] = seq
     np_arr[:, 1] = files_list
 
-    with open('../ImageSets/train.txt', 'a') as f:
+    with open(base_path + 'ImageSets/train.txt', 'a') as f:
         np.savetxt(f, np_arr, delimiter=" ", fmt='%s')
 
 # Generating val.txt file with the given sequences
 
 # removing existing file
-os.remove('../ImageSets/val.txt')
+test_file_path = base_path + 'ImageSets/val.txt'
+if os.path.exists(test_file_path):
+    os.remove(test_file_path)
 
 for seq in selected_val_seq:
 
-    seq_path = "../training/velodyne/%s/"%(seq)
+    seq_path = base_path + "training/velodyne/%s/"%(seq)
     files_list = list(map(get_file_name, os.listdir(seq_path)))
 
     np_arr = np.empty((len(files_list), 2), dtype="object")
     np_arr[:, 0] = seq
     np_arr[:, 1] = files_list
 
-    with open('../ImageSets/val.txt', 'a') as f:
+    with open(base_path + 'ImageSets/val.txt', 'a') as f:
         np.savetxt(f, np_arr, delimiter=" ", fmt='%s')
